3d4866979e682a733fc6267e69e1e9a6734aea09
Nita-Thapa-Magar <ntm2057@gmail.com>
2022-05-01 21:16:47 +0545

    Function for taking input from players is defined.

--- tictactoe.py
+++ tictactoe.py
@@ -13,4 +13,17 @@ def printBoard(board):
     print(board[3] + " | " + board[4] + " | " + board[5])
     print("---------")
     print(board[6] + " | " + board[7] + " | " + board[8])
-printBoard(board)
+#printBoard(board)
+
+# take player input
+def playerInput(board):
+    inp = int(input("Enter a number from 1-9: "))
+    if inp >=1 and inp<= 9 and board[inp-1] == "-":
+        board[inp-1] = currentPlayer
+    else:
+        print("The player is already at that spot.")
+
+
+while gameRunning:
+    printBoard(board)
+    playerInput(board)
