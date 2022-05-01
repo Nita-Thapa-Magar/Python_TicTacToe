c91ec8b889418ef4cf17c5d0915dcf291db148ca
Nita-Thapa-Magar <ntm2057@gmail.com>
2022-05-01 21:07:27 +0545

    First Commit

---
+++ tictactoe.py
@@ -0,0 +1,16 @@
+board = ["-", "-", "-",
+        "-", "-", "-",
+        "-", "-", "-"]
+        
+currentPlayer = "X"
+winner = None
+gameRunning = True
+
+# game board
+def printBoard(board):
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print("---------")
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print("---------")
+    print(board[6] + " | " + board[7] + " | " + board[8])
+printBoard(board)
